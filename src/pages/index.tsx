010a9cf96fef38ac2550c733c810de9126827eb4
Rahayumkdpt <145326487+Rahayumkdpt@users.noreply.github.com>
2024-05-13 11:44:23 +0800

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,10 @@
 import SplashScreen from './SplashScreen';
 import Menu from './Menu';
 import Female from './Female';
+import Male from './Male';
+import Simp from './Simp';
+import Artt from './Artt';
+//import routerFem from './Female/routerFem';
 //import Home from './Home';
 
-export {SplashScreen, Menu, Female}; 
+export {SplashScreen, Menu, Female, Male, Simp, Artt};
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {SplashScreen, Menu, Female} from '../pages';
-
+import {SplashScreen, Menu} from '../pages';
+import Female from '../pages/Female';
+import Male from '../pages/Male';
+import Simp from '../pages/Simp';
+import Artt from '../pages/Artt';
 const Stack = createNativeStackNavigator();
 
 const index = () => {
@@ -16,13 +19,29 @@ const index = () => {
         name="Menu"
         component={Menu}
         options={{headerShown: false}}
-      /> 
+      />
       <Stack.Screen
         name="Female"
         component={Female}
         options={{headerShown: false}}
       />
-    
+      <Stack.Screen
+        name="Simp"
+        component={Simp}
+        options={{headerShown: false}}
+      />
+       <Stack.Screen
+        name="Artt"
+        component={Artt}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Male"
+        component={Male}
+        options={{headerShown: false}}
+      />
+      
+     
     </Stack.Navigator>
   );
 };
